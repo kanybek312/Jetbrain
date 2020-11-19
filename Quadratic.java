f0e6372b5422f4e30fde5f37c73d39e9cc3f73c4
Kanybek Mukalaev <m.kanybek@mail.ru>
2020-11-19 23:57:03 +0100

    working with math

---
+++ Quadratic.java
@@ -0,0 +1,28 @@
+package Jetbrain.Math;
+
+import java.util.Scanner;
+
+public class Quadratic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextDouble()
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        double d;
+        double x1;
+        double x2;
+        if (a != 0){
+        d = Math.sqrt(Math.pow(b, 2) - (4) * (a) * (c));
+        x1 = (-b + d) / (2 * a);
+        x2 = (-b - d) / (2 * a);
+
+        if ( x1 < x2) {
+            System.out.println(x1);
+            System.out.println(x2);
+        } else {
+            System.out.println(x2);
+            System.out.println(x1);
+        }
+        }
+    }
+}
